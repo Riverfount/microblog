81592f3c71dd7e1a881c3c81dddcafa78f73ad4b
Vicente Marçal <vicente.marcal@gmail.com>
2020-02-03 12:23:28 -0300

    Did tasks of 4th Chapter

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
-from flask import render_template
+from flask import (
+    flash,
+    redirect,
+    render_template,
+    url_for,
+)
 
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -18,3 +24,12 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user: {form.username.data}, remember me: {form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login_form.html', title='Sign In', form=form)
